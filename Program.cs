48141bb3fb0b7d92a35b0d1419ca56efff900f29
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-06 19:50:17 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace workingwithnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 8);
+            Console.WriteLine(5 - 3);
+            Console.WriteLine(5 * 5);
+            Console.WriteLine(100 / 10);
+            Console.WriteLine( 5 + 4 * 3);
+            Console.WriteLine((5 + 4) * 3);
+
+            int num = 6;
+            num++;
+            Console.WriteLine(num);
+            num--;
+            Console.WriteLine(num);
+
+            //math methods
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(5.2, 8));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(50, 100));
+            Console.WriteLine(Math.Min(50, 100));   
+            Console.WriteLine(Math.Round(5.6));
+
+            Console.ReadLine();
+        }
+    }
+}
